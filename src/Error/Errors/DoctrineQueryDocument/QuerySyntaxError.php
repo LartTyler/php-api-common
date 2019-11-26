a133f215737396ce7dfb8e75a175056bb577c3d9
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-11-26 18:27:13 -0500

    Fix incorrect variable being passed to parent constructor

--- src/Error/Errors/DoctrineQueryDocument/QuerySyntaxError.php
+++ src/Error/Errors/DoctrineQueryDocument/QuerySyntaxError.php
@@ -14,6 +14,6 @@
 		public function __construct(?string $error = null, ?int $httpStatus = Response::HTTP_BAD_REQUEST) {
 			$message = 'Your query object is invalid; ' . ($error ?? 'check your syntax and try again');
 
-			parent::__construct('invalid_query.syntax', $error, $httpStatus);
+			parent::__construct('invalid_query.syntax', $message, $httpStatus);
 		}
 	}
