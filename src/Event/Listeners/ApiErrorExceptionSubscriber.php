865872571c9d30717e2d0c2ed0b00eeeb2549fbc
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-12-03 15:38:58 -0500

    Add subscriber to convert uncaught ApiErrorExceptions to responses

---
+++ src/Event/Listeners/ApiErrorExceptionSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+	namespace DaybreakStudios\RestApiCommon\Event\Listeners;
+
+	use DaybreakStudios\RestApiCommon\Exceptions\ApiErrorException;
+	use DaybreakStudios\RestApiCommon\ResponderService;
+	use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+	use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+	class ApiErrorExceptionSubscriber implements EventSubscriberInterface {
+		/**
+		 * @var ResponderService
+		 */
+		protected $responder;
+
+		/**
+		 * ApiErrorExceptionSubscriber constructor.
+		 *
+		 * @param ResponderService $responder
+		 */
+		public function __construct(ResponderService $responder) {
+			$this->responder = $responder;
+		}
+
+		/**
+		 * {@inheritdoc}
+		 */
+		public static function getSubscribedEvents(): array {
+			return [
+				'kernel.exception' => 'onKernelException',
+			];
+		}
+
+		/**
+		 * @param ExceptionEvent $event
+		 */
+		public function onKernelException(ExceptionEvent $event): void {
+			$exception = $event->getThrowable();
+
+			if ($exception instanceof ApiErrorException)
+				$event->setResponse($this->responder->createErrorResponse($exception->getApiError()));
+		}
+	}
